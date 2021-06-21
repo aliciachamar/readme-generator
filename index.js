0bc0b58db417e8d5f23cd2b16da230aa66395a5d
aliciachamar <aliciachamar@gmail.com>
2021-06-21 11:32:03 -0600

    added questions for user input

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?",
+    },
+    {
+        type: "input",
+        name: "desc",
+        message: "Please enter a description of your project.",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    const data = inquirer.prompt(questions);
+    
+}
 
 // Function call to initialize app
 init();
